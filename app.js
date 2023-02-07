4fbeeb54f934476f05a8b712e4545a3bda0399e5
unknown <kostunja@rambler.ru>
2023-02-07 14:06:03 +0300

    kostya1

--- app.js
+++ app.js
@@ -1,73 +1,75 @@
-let name = document.querySelector('#name');
-let price = document.querySelector('#price');
+let name   = document.querySelector('#name');
+let price  = document.querySelector('#price');
 let amount = document.querySelector('#amount');
-let add = document.querySelector('#add');
-let table = document.querySelector('#table');
-let total = document.querySelector('#total');
+let add    = document.querySelector('#add');
+let table  = document.querySelector('#table');
+let total  = document.querySelector('#total');
 
 add.addEventListener('click', function() {
 	let tr = document.createElement('tr');
-
-	let td;
-
-	td = document.createElement('td');
-	td.textContent = name.value;
-	td.classList.add('name');
-	tr.appendChild(td);
-
-	td = document.createElement('td');
-	td.textContent = price.value;
-	td.classList.add('price');
-	tr.appendChild(td);
-
-	td = document.createElement('td');
-	td.textContent = amount.value;
-	td.classList.add('amount');
-	tr.appendChild(td);
-
-	td = document.createElement('td');
-	td.textContent = price.value * amount.value;
-	td.classList.add('cost');
-	tr.appendChild(td);
-
-	td = document.createElement('td');
-	td.textContent = 'удалить';
-	td.classList.add('remove');
-	tr.appendChild(td);
-
+	
+	allowEdit(createCell(tr, name.value, 'name'));
+	allowEdit(createCell(tr, price.value, 'price'));
+	allowEdit(createCell(tr, amount.value, 'amount'));
+	
+	createCell(tr, price.value * amount.value, 'cost');
+	
+	createCell(tr, 'удалить', 'remove').addEventListener('click', function() {
+		this.parentElement.parentElement.removeChild(this.parentElement);
+		recountTotal();
+	});
+	
 	table.appendChild(tr);
+	recountTotal();
 });
 
 function createCell(tr, value, name) {
-	add.addEventListener('click', function() {
-		let tr = document.createElement('tr');
-
-		createCell(tr, name.value, 'name');
-		createCell(tr, price.value, 'price');
-		createCell(tr, amount.value, 'amount');
-		createCell(tr, price.value * amount.value, 'cost');
-		createCell(tr, 'удалить', 'remove');
+	let td = document.createElement('td');
+	td.textContent = value;
+	td.classList.add(name);
+	tr.appendChild(td);
 	
-		table.appendChild(tr);
+	return td;
+}
+
+function allowEdit(td) {
+	td.addEventListener('dblclick', function() {
+		let text = td.textContent
+		td.textContent = '';
+		
+		let input = document.createElement('input');
+		input.value = text;
+		input.focus();
+		td.appendChild(input);
+		
+		input.addEventListener('keydown', function(event) {
+			if (event.key == 'Enter') {
+				td.textContent = this.value;
+				
+				if (td.classList.contains('price') || td.classList.contains('amount')) {
+					let tr = td.parentElement;
+					let price = tr.querySelector('.price');
+					let amount = tr.querySelector('.amount');
+					let cost = tr.querySelector('.cost');
+					
+					cost.textContent = price.textContent * amount.textContent;
+					recountTotal();
+				}
+			}
+		});
 	});
 }
 
-add.addEventListener('click', function() {
-	let tr = document.createElement('tr');
-
-	createCell(tr, name.value, 'name');
-	createCell(tr, price.value, 'price');
-	createCell(tr, amount.value, 'amount');
-	createCell(tr, price.value * amount.value, 'cost');
-	createCell(tr, 'удалить', 'remove');
-
-	table.appendChild(tr);
-	recountTotal(); 
-});
-
 function recountTotal() {
 	let costs = table.querySelectorAll('.cost');
+	
 	if (costs) {
-
+		let sum = 0;
+		
+		for (let cost of costs) {
+			sum += +cost.textContent;
+		}
+		
+		total.textContent = sum;
 	}
 }
